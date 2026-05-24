49761b37cf1d929e679804d4aea2cc8ab967307c
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2026-05-24 16:40:01 +0800

    docs: add file header comment to Profile.swift

--- SwiftUI官网学习/5.玩转UI控件/Landmarks/Models/Profile.swift
+++ SwiftUI官网学习/5.玩转UI控件/Landmarks/Models/Profile.swift
@@ -1,3 +1,12 @@
+//
+//  Profile.swift
+//  SwiftUI-Study
+//
+//  Created by MoonStartMan on 2024.
+//  SwiftUI玩转UI控件：用户档案数据模型
+//
+//  详细说明这个文件的作用和主要功能
+//
 /*
 See LICENSE folder for this sample’s licensing information.
 
