c7ed8a670b35a511dc434346ec6c7c04c5e72f9e
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2026-05-24 16:39:58 +0800

    docs: add file header comment to Landmark.swift

--- SwiftUI官网学习/5.玩转UI控件/Landmarks/Models/Landmark.swift
+++ SwiftUI官网学习/5.玩转UI控件/Landmarks/Models/Landmark.swift
@@ -1,3 +1,12 @@
+//
+//  Landmark.swift
+//  SwiftUI-Study
+//
+//  Created by MoonStartMan on 2024.
+//  SwiftUI玩转UI控件：地标数据模型
+//
+//  详细说明这个文件的作用和主要功能
+//
 /*
 See LICENSE folder for this sample’s licensing information.
 
