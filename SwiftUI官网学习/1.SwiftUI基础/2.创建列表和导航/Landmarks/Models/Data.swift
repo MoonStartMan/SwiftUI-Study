1c598506be78330793260a9a9a647bf13124e71d
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2026-05-24 16:39:12 +0800

    docs: add file header comment to Data.swift

--- SwiftUI官网学习/1.SwiftUI基础/2.创建列表和导航/Landmarks/Models/Data.swift
+++ SwiftUI官网学习/1.SwiftUI基础/2.创建列表和导航/Landmarks/Models/Data.swift
@@ -1,3 +1,12 @@
+//
+//  Data.swift
+//  SwiftUI-Study
+//
+//  Created by MoonStartMan on 2024.
+//  SwiftUI基础-列表导航：数据模型定义
+//
+//  详细说明这个文件的作用和主要功能
+//
 /*
 See LICENSE folder for this sample’s licensing information.
 
