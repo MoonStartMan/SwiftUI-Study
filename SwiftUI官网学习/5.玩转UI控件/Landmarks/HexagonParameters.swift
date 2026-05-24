b555b13981e6a4329d1de44840e6931a389740af
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2026-05-24 16:39:51 +0800

    docs: add file header comment to HexagonParameters.swift

--- SwiftUI官网学习/5.玩转UI控件/Landmarks/HexagonParameters.swift
+++ SwiftUI官网学习/5.玩转UI控件/Landmarks/HexagonParameters.swift
@@ -1,3 +1,12 @@
+//
+//  HexagonParameters.swift
+//  SwiftUI-Study
+//
+//  Created by MoonStartMan on 2024.
+//  SwiftUI玩转UI控件：六边形路径参数定义
+//
+//  详细说明这个文件的作用和主要功能
+//
 /*
 See LICENSE folder for this sample’s licensing information.
 
